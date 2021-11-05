d02eed0644f88f6f76f747c8a18b3253a565b324
Vladimir <zelleaz@mail.ru>
2021-11-05 19:48:10 +0500

    remove unused attributes from Api class

--- src/api/api.js
+++ src/api/api.js
@@ -1,9 +1,3 @@
-import axios from "axios";
-
 export class Api {
     baseUrl = 'https://dispex.org/api/vtest'
-    requestUrl = this.baseUrl + '/Request'
-    housingStockUrl = this.baseUrl + '/HousingStock'
-
-
 }

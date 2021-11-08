6ee9b003406ab6be18b016649885cd71ad28b3e9
Vladimir <zelleaz@mail.ru>
2021-11-08 15:24:54 +0500

    not done. Won't finish because code is done on another computer (can't commit to git)

--- src/Components/Users/DeleteUser.jsx
+++ src/Components/Users/DeleteUser.jsx
@@ -4,12 +4,12 @@ import ApiHousingStock from "../../api/ApiHousingStock";
 import {useDispatch, useSelector} from "react-redux";
 import {ClientsActionCreators} from "../../Store/clientsReducer/clientsActionCreators";
 
-const DeleteUser = ({ id }) => {
+const DeleteUser = ({ bindId }) => {
     const dispatch = useDispatch()
     const { flat } = useSelector(state => state?.main)
 
     const onClick = async () => {
-        await ApiHousingStock.unBindClient(id)
+        await ApiHousingStock.unBindClient(bindId)
         await dispatch(ClientsActionCreators.fetchClients(flat.id))
     }
 
